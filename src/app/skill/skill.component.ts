230188c517f49f6fc2b2bbaf5d5f173385810081
Abir-97 <mhabirkhan97@gmail.com>
2023-05-14 17:24:23 +0600

    Changes

--- src/app/skill/skill.component.ts
+++ src/app/skill/skill.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, collectionData, deleteDoc, doc, updateDoc } from '@angular/fire/firestore';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 
@@ -15,16 +15,74 @@ export class SkillComponent {
   SkillId='';
   constructor(private _fireStore:Firestore,private _fb :FormBuilder) {
    this.SkillEntrty=_fb.group({
-    
+    Project:'',
+    ModuleName:'',
+    Sort:0,
    })
   }
 
-  onSave(){
+  ngOnInit():void {
+    this.GetAll();
+  }
 
+  onSave(){
+    if(this.SkillId=='' || this.SkillId==undefined){
+      this.SaveData();
+      this.SkillId=='';
+      this.saveBtn='Save';
+    }else{
+      this.updateData();
+      this.SkillId=='';
+      this.saveBtn='Save';
+    }
+  
   }
 
   ResetBtn(){
-    
+    this.SkillEntrty.reset();
+    this.saveBtn='Save';
   }
 
+  SaveData(){
+    const instance=collection(this._fireStore,'Skills');
+    addDoc(instance,this.SkillEntrty.value).then(()=>{
+      console.log('Save Data');
+    }).then((error)=>{
+      console.log(error);
+    });
+    this.SkillEntrty.reset();
+  }
+
+  GetAll(){
+     const instance=collection(this._fireStore,'Skills');
+     collectionData(instance,{idField:'id'}).subscribe((data)=>{
+
+     })
+     this.SkillsList= collectionData(instance,{idField:'id'});
+  }
+
+  Edit(data:any){
+    this.saveBtn='Update';
+     this.SkillEntrty.patchValue(data);
+     this.SkillId=data.id;
+  }
+
+  updateData(){
+    const instance=doc(this._fireStore,'Skills',this.SkillId);
+    updateDoc(instance,this.SkillEntrty.value).then(()=>{
+      console.log('Update Data');
+    }).then((error)=>{
+      console.log(error);
+    });
+    this.SkillEntrty.reset();
+  }
+
+  Delete(id:any){
+    const instance=doc(this._fireStore,'Skills',id.id);
+    deleteDoc(instance).then(()=>{
+      console.log('Delete Success');
+    }).then((error)=>{
+      console.log(error);
+    })
+  }
 }
